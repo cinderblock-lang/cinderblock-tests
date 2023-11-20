070ced887a9d39c0f2d1ef775835f8373e61c33e
Paul Popat <paul.popat@novuna.co.uk>
2023-11-20 09:01:01 +0000

    Cleaned up test code

--- test.sh
+++ test.sh
@@ -4,23 +4,29 @@ errors=false;
 
 run_code() {
   dir=$PWD;
-  cd $1;
+  cd "$1" || exit;
   cinderblock compile
-  eval "./bin/linux/$2"
+  stdout=$(eval "./bin/linux/$2")
 
   resp=$?;
   echo $resp
 
-  if [ $resp -ne $3 ]; then
+  if [ "$resp" != "$3" ]; then
     errors=true;
     echo "Unexpected result from $1";
   fi
 
-  cd $dir;
+  if [ "$stdout" != "$4" ]; then
+    errors=true;
+    printf "Unexpected stdout from %s\nExpected:\n%s\nReceived:\n%s\n" "$1" "$4" "$stdout"
+  fi
+
+  cd "$dir" || exit;
 }
 
-run_code 'hello-world' 'hello_world' 0
-run_code 'simple-maths' 'simple_maths' 4
+run_code 'hello-world' 'hello_world' 0 ""
+run_code 'simple-maths' 'simple_maths' 4 ""
+run_code 'externals' 'externals' 12 "Hello world"
 
 if [ $errors = true ]; then
   echo "You had some errors. Check the logs above.";
