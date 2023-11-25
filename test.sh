411c803fea1f89306ee82af7dde44055bc42d3a2
Paul Popat <paul@popatux.co.uk>
2023-11-25 21:43:50 +0000

    Made better tests

--- test.sh
+++ test.sh
@@ -3,6 +3,7 @@
 errors=false;
 
 run_code() {
+  printf "Running test for $1\n"
   dir=$PWD;
   cd "$1" || exit;
   cinderblock compile
@@ -12,20 +13,22 @@ run_code() {
 
   if [ "$resp" != "$3" ]; then
     errors=true;
-    echo "Unexpected result from $1";
+    printf "Unexpected result from %s\nExpected %s\nReceived: %s\n\n" "$1" "$3" "$resp";
   fi
 
   if [ "$stdout" != "$4" ]; then
     errors=true;
-    printf "Unexpected stdout from %s\nExpected:\n%s\nReceived:\n%s\n" "$1" "$4" "$stdout"
+    printf "Unexpected stdout from %s\nExpected:\n%s\nReceived:\n%s\n\n" "$1" "$4" "$stdout"
   fi
 
   cd "$dir" || exit;
+  printf "\n\n\n"
 }
 
 run_code 'hello-world' 'hello_world' 0 ""
 run_code 'simple-maths' 'simple_maths' 4 ""
 run_code 'externals' 'externals' 12 "Hello world"
+run_code 'functions' 'functions' 24 "This is from a function"
 run_code 'lambdas' 'lambdas' 20 "This is in a lambda"
 
 if [ $errors = true ]; then
